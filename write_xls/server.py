7b6bde8594aaa3cf88b64af2ed8882dde1e328bb
Shivnaren Srinivasan <shivnaren@gmail.com>
2023-12-25 22:26:39 +0530

    Added index endpoint

--- write_xls/server.py
+++ write_xls/server.py
@@ -8,17 +8,9 @@ import pandas as pd
 app = fastapi.FastAPI()
 
 
-@app.get("/_xlsx")
-def get_excel_data():
-    df = pd.DataFrame([["Canada", 10], ["USA", 20]], columns=["team", "points"])
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer) as writer:
-        df.to_excel(writer, index=False)
-    return StreamingResponse(
-        io.BytesIO(buffer.getvalue()),
-        media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        headers={"Content-Disposition": f"attachment; filename=data.xlsx"},
-    )
+@app.get('/')
+async def index() -> str:
+    return 'hello'
 
 
 @app.post("/xlsx")
